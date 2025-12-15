c668beed8d48dc77d3f45c72c031c9f69001674a
Juan Dávila <davilajuan926@gmail.com>
2025-12-14 19:40:41 -0500

    Project deployed

--- server/src/app.js
+++ server/src/app.js
@@ -26,8 +26,8 @@ app.use(router)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
 
